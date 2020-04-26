c09b7bddc523168f0e6c3e9547a67142327c5130
Abousaid <abousaid7salah@gmail.com>
2020-04-26 18:06:37 +0000

    first commit

---
+++ client/build/precache-manifest.2b51d56b59e61ec9936587c4955d4f0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fe0535405d2a9b9aa9e75cb8afda6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d6a4fe43d122c52f820",
+    "url": "/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "fcb1029d74144164bee3",
+    "url": "/static/js/2.9632fcdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9632fcdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d6a4fe43d122c52f820",
+    "url": "/static/js/main.923dc8d1.chunk.js"
+  },
+  {
+    "revision": "51a518e45330f669e104",
+    "url": "/static/js/runtime-main.51eec7e2.js"
+  }
+]);
---
+++ client/src/components/Balance.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { numberWithCommas } from '../utils/format';
+
+export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  console.log(amounts);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0);
+
+  console.log(total);
+
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>${numberWithCommas(total)}</h1>
+    </>
+  );
+};
